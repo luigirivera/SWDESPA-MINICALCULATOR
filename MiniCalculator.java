3506b1845cac34cd9e34a5ccd287e6d3b323aa0d
Luis Lopez <luis_lopez@dlsu.edu.ph>
2018-01-30 21:42:20 +0800

    Fixed MiniCalculator.java, added .gitignore

--- MiniCalculator.java
+++ MiniCalculator.java
@@ -1,70 +1,75 @@
 import java.util.Scanner;
 
 interface Computor{
-	public int compute(int val1, int val2);
+	public String compute(float val1, float val2);
+}
+
+class Invalid implements Computor{
+    public String compute(float val1, float val2)
+    {
+        return "ERROR: Invalid operation";
+    }
 }
 
 class Add implements Computor{
-	public int compute(int val1, int val2)
+	public String compute(float val1, float val2)
 	{
-		return val1 + val2;
+		return String.valueOf((int)(val1 + val2));
 	}
 }
 
 class Subtract implements Computor{
-	public int compute(int val1, int val2)
+	public String compute(float val1, float val2)
 	{
-		return val1 - val2;
+		return String.valueOf((int)(val1 - val2));
 	}
 }
 
 class Multiply implements Computor{
-	public int compute(int val1, int val2)
+	public String compute(float val1, float val2)
 	{
-		return val1 * val2;
+		return String.valueOf((int)(val1 * val2));
 	}
 }
 
 class Divide implements Computor{
-	public int compute(int val1, int val2)
+	public String compute(float val1, float val2)
 	{
-		try
-		{
-			return (int) val1 / val2;
-		}
-		catch(ArithmeticException e)
-		{
-			if(val2 == 0)
-				System.out.println("ERROR: You can not divide by 0");
-		}
+        if (val2==0)
+            return "ERROR: Can't divide by 0";
+        else
+            return String.valueOf((int)(val1/val2));
 	}
 }
 
+class Parser{
+    
+}
+
 class Calculator{
-	Scanner input = new Scanner(System.in);
-	String expression;
-	String[] operation;
-	String symbol;
-	Computor operator;
-	int[] values = new int[2];
-	
 	public void calculatorStart()
 	{
+        String expression;
+        Scanner input = new Scanner(System.in);
+        String[] parsedExpression = new String[2];
 		do
 		{
-			System.out.print("Please enter your expression or enter '***' to exit: ");
 			expression = input.nextLine();
 			expression = expression.trim();
 			if(!expression.equals(Symbols.EXIT.toString()))
 			{
-				System.out.print("Result: ");
-				
-				operation = expression.split("\\s", 3);
-				symbol = operation[1];
-				values[0] = Integer.parseInt(operation[0]);
-				values[1] = Integer.parseInt(operation[2]);
-				findOperation();
-				
+                //placing space between every character
+                expression = expression.replace("", " ");
+                //removing extra space between numbers joined by decimal point
+                expression = expression.replaceAll("[\\.][\\s]", ".");
+                expression = expression.replaceAll("[\\s][\\.]", ".");
+                //removing extra space between digits of a number
+                expression = expression.replaceAll("([0-9])([\\s])", "$1");
+                //removing extra space on the sides
+                expression = expression.trim();
+                //split expression into 3 strings using whitespace as splitter
+				parsedExpression = expression.split("\\s+", 3);
+				printResult(Float.parseFloat(parsedExpression[0]), Float.parseFloat(parsedExpression[2]), findComputor(parsedExpression[1]));
 			}
 				
 		}while(!expression.equals(Symbols.EXIT.toString()));
@@ -72,30 +77,24 @@ class Calculator{
 		input.close();
 	}
 	
-	private void findOperation()
-	{
-		Add addi = new Add();
-		Subtract sub = new Subtract();
-		Multiply mul = new Multiply();
-		Divide div = new Divide();
-		
-		if(symbol.equals(Symbols.ADD.toString()))
-			operator = addi; 
-		else if(symbol.equals(Symbols.SUB.toString()))
-			operator = sub;
-		else if(symbol.equals(Symbols.MULT.toString()))
-			operator = mul;
-		else if(symbol.equals(Symbols.DIV.toString()))
-			operator = div;
-		else
-			System.out.println("ERROR: Invalid Operation");
-		
-		doOperation();
+	private Computor findComputor(String symbol) {
+        /* Why not switch statement?
+            Switch cases would have needed "constant string expression" */
+        if (symbol.equals(Symbols.ADD.toString()))
+            return new Add();
+        else if (symbol.equals(Symbols.SUB.toString()))
+            return new Subtract();
+        else if (symbol.equals(Symbols.MULT.toString()))
+            return new Multiply();
+        else if (symbol.equals(Symbols.DIV.toString()))
+            return new Divide();
+        else
+            return new Invalid();
 	}
 	
-	private void doOperation()
+	private void printResult(float val1, float val2, Computor computor)
 	{
-		System.out.println(operator.compute(values[0], values[1]));
+		System.out.println(computor.compute(val1, val2));
 	}
 	
 }
