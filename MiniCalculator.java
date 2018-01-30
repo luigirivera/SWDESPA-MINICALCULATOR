ff9f77ef8436bcf02c3dfe8f745a25d044d01435
Luis Lopez <luis_lopez@dlsu.edu.ph>
2018-01-30 22:13:29 +0800

    Fixed some more

--- MiniCalculator.java
+++ MiniCalculator.java
@@ -42,33 +42,44 @@ class Divide implements Computor{
 	}
 }
 
-class Parser{
-    
+class ExpressionParser{
+    public String[] parse(String raw)
+    {
+        //placing space between every character
+        raw = raw.replace("", " ");
+        System.out.println(raw);
+        //removing extra space between numbers joined by decimal point
+        raw = raw.replaceAll("[\\.][\\s]", ".");
+        raw = raw.replaceAll("[\\s][\\.]", ".");
+        System.out.println(raw);
+        //removing extra space between digits of a number
+        raw = raw.replaceAll("([0-9])([\\s])([0-9])", "$1$3");
+        System.out.println(raw);
+        //removing extra space on the sides
+        raw = raw.trim();
+        System.out.println(raw);
+        //split expression into 3 strings using whitespace as splitter
+        return raw.split("\\s+", 3);
+    }
 }
 
 class Calculator{
 	public void calculatorStart()
 	{
         String expression;
+        String[] parsedExpression;
         Scanner input = new Scanner(System.in);
-        String[] parsedExpression = new String[2];
+        ExpressionParser parser = new ExpressionParser();
 		do
 		{
 			expression = input.nextLine();
 			expression = expression.trim();
 			if(!expression.equals(Symbols.EXIT.toString()))
 			{
-                //placing space between every character
-                expression = expression.replace("", " ");
-                //removing extra space between numbers joined by decimal point
-                expression = expression.replaceAll("[\\.][\\s]", ".");
-                expression = expression.replaceAll("[\\s][\\.]", ".");
-                //removing extra space between digits of a number
-                expression = expression.replaceAll("([0-9])([\\s])", "$1");
-                //removing extra space on the sides
-                expression = expression.trim();
-                //split expression into 3 strings using whitespace as splitter
-				parsedExpression = expression.split("\\s+", 3);
+                parsedExpression = parser.parse(expression);
+                System.out.println(parsedExpression[0]);
+                System.out.println(parsedExpression[1]);
+                System.out.println(parsedExpression[2]);
 				printResult(Float.parseFloat(parsedExpression[0]), Float.parseFloat(parsedExpression[2]), findComputor(parsedExpression[1]));
 			}
 				
